32bd186b34287aa7c691267dad6270b389f38f23
DMarriott92 <104208603+DMarriott92@users.noreply.github.com>
2025-11-21 12:44:05 +0000

    Completed Circular Queue w/12

--- queue.py
+++ queue.py
@@ -7,10 +7,40 @@ class CircularQueue:
         self.size = 0
         self.maxSize = maxItems
     
+    def enQueue(self, item):
+        if self.size == self.maxSize:
+            print("Queue is full, size =", self.size)
+        else:
+            self.rear = (self.rear + 1)%(self.maxSize)
+            self.size += 1
+            self.items[self.rear] = item
+
+
+    def deQueue(self):
+        if (self.size == 0):
+            return "Queue Empty"
+        else:
+            first = self.items[self.front]
+            self.size -= 1
+            self.front = (self.front + 1)%(self.maxSize)
+            return first
+
+    def isFull(self):
+        print("self.size, self.maxSize", self.size, self.maxSize)
+        return self.size == self.maxSize
+
+    def isempty(self):
+        print("self.size, self.maxSize", self.size, self.maxSize)
+        return self.size == 0
+
     def show(self):
         print("lists and pointers", self.iitems, self.front, self.rear, "size =",self.size)
 
     def size(self):
         return self.size
+    
+    def showFront(self):
+        print(self.items[self.front])
 
-q = CircularQueue(5)
+q = CircularQueue(10)
+q.enQueue("320")
